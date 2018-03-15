c6592c043320a4701a1009eeb713ff78b8fd746b
Igor Crispim Diniz <igor.crispimdiniz@gmail.com>
2018-03-15 03:38:40 -0300

    Minor configurations corrections

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   githubOauthUrl: clientId =>
     `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=read:user%20user:email%20admin:repo_hook`,
-  githubOauthRedirectUrl: 'https://shepherdy.herokuapp.com/authorization',
+  githubOauthRedirectUrl: 'https://shepherdy.herokuapp.com/authentication',
   clientId: '213e2fe09d8d983e11d8',
 };
 
